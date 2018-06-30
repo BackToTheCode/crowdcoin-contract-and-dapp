4b928c4feeac417e86c57fc91564393ea090535a
James Tuckett <james@wearepoint.com>
2018-06-30 14:35:50 +0100

    Add semantic ui react and storybook react

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import factory from '../ethereum/factory';
+import { Card } from 'semantic-ui-react';
+// import 'semantic-ui-css/semantic.min.css';
 
 class CampaignIndex extends Component {
-    async componentDidMount() {
+
+    static async getInitialProps() {
         const campaigns = await factory.methods.getDeployedCampaigns().call();
-        console.log(campaigns);
+        return { campaigns };
+    }
+
+    renderCampaigns() {
+        const items = this.props.campaigns.map(address => {
+            return {
+                header: address,
+                description: <a>View campaign</a>,
+                fluid: true
+            }
+        });
+
+        return <Card.Group items={items} />;
+
     }
 
     render() {
         return (
-            <h1>Campaign Page!</h1>
+            <div>
+                <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css"></link>
+                <h1>{this.renderCampaigns()}</h1>
+            </div>
         )
     }
 }
